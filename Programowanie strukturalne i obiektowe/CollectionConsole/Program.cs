ee5bb7935eab15702fed1d9406a7a1b4966c996d
essa <essa@gmail.com>
2023-01-17 09:19:52 +0100

    C#

---
+++ Programowanie strukturalne i obiektowe/CollectionConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CollectionConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCollection testCollection = new TestCollection();
+            // testCollection.TestArrayCollection();
+            //testCollection.TestObjectArrayCollection();
+
+            TestListCollection testListCollection = new TestListCollection();
+            testListCollection.FirstTest();
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/CollectionConsole/TestListOfObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionConsoleApp
+{
+    class TestListOfObject
+    {
+        public void Test()
+        {
+            List<Person> listOfPeople = new List<Person>();
+
+            Person person = new Person("Jan", "Kowalski", 40, 170);
+            listOfPeople.Add(person);
+
+            listOfPeople.Add(new Person("Ewa", "Nowak", 36, 150));
+            listOfPeople.Add(new Person("Paweł", "Nieznany", 15, 120));
+            listOfPeople.Add(new Person("Bez", "Imienny", 14, 180));
+
+            Console.WriteLine("Lista wszystkich osób");
+            foreach (Person p in listOfPeople)
+            {
+                p.Info();
+            }
+
+            List<Person> listofPeoplePegi18;
+
+            listofPeoplePegi18 = listOfPeople.Where(x => x.age >= 18).ToList();
+
+            Console.WriteLine("Lista osób pełnoletnich");
+            foreach (Person p in listofPeoplePegi18)
+            {
+                p.Info();
+            }
+
+            List<Person> sortedListByAge = listOfPeople.OrderBy(x => x.age ).ToList();
+            Console.WriteLine("Lista osób posortowana według wieku");
+            foreach (Person p in sortedListByAge)
+            {
+                p.Info();
+            }
+            Console.WriteLine();
+
+
+        }
+
+    }
+}
